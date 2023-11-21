f8eb0d90d8cfb6802d1735fc4027075a7609776e
bshongwe <dru.da.beat.hustle@gmail.com>
2023-11-20 22:59:50 -0800

    Task 0 - code label

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * bubble_sort - Function uses bubble sort algorithm
@@ -16,11 +17,15 @@ void bubble_sort(int *array, size_t size)
 		return;
 	}
 
+	/* Loop counter */
 	i = 0;
+	/* Outer loop */
 	while (i < size)
 	{
+		/* Inner loop */
 		for (z = 0; z < size - 1; z++)
 		{
+			/* Comparing elements */
 			if (array[z] > array[z + 1])
 			{
 				tmp = array[z];
