f64da133dca0b03244ba513d4bc9ddb98f705d5f
bshongwe <dru.da.beat.hustle@gmail.com>
2023-11-23 02:13:24 -0800

    Task 10 - Advanced

---
+++ 106-bitonic_sort.c
@@ -0,0 +1,87 @@
+#include "sort.h"
+
+/**
+ * swap_integers - Func swaps two integers in an array.
+ * @a: The first integer.
+ * @b: The second integer.
+ */
+void swap_integers(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bitonic_merge - Func sorts bitonic sequence in integers array.
+ * @array: Input array of integers.
+ * @size: Size of array.
+ * @start: Starting index of sequence in array to sort.
+ * @seq: The size of the sequence to sort.
+ * @flow: The direction to sort in.
+ */
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow)
+{
+	size_t i, jump = seq / 2;
+
+	if (seq > 1)
+	{
+		for (i = start; i < start + jump; i++)
+		{
+			if ((flow == UP && array[i] > array[i + jump]) ||
+			    (flow == DOWN && array[i] < array[i + jump]))
+				swap_integers(array + i, array + i + jump);
+		}
+		bitonic_merge(array, size, start, jump, flow);
+		bitonic_merge(array, size, start + jump, jump, flow);
+	}
+}
+
+/**
+ * bitonic_seq - Func converts array of integers into bitonic sequence.
+ * @array: Iput array of integers.
+ * @size: Size of array.
+ * @start: Starting index of block for building bitonic sequence.
+ * @seq: Block size for building bitonic sequence.
+ * @flow: Bitonic sequence block sorting direction.
+ */
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
+{
+	size_t cut = seq / 2;
+	char *str = (flow == UP) ? "UP" : "DOWN";
+
+	if (seq > 1)
+	{
+		printf("Merging [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+
+		bitonic_seq(array, size, start, cut, UP);
+		bitonic_seq(array, size, start + cut, cut, DOWN);
+		bitonic_merge(array, size, start, seq, flow);
+
+		printf("Result [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+	}
+}
+
+/**
+ * bitonic_sort - Func uses bitonic sort algorithm to sort
+ *                an array of integers in ascending order.
+ * @array: Input array of integers.
+ * @size: Size of array.
+ *
+ * Description: Prints the array after each swap. Only works for
+ * size = 2^k where k >= 0 (ie. size equal to powers of 2).
+ */
+void bitonic_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
+	bitonic_seq(array, size, 0, size, UP);
+}
--- sort.h
+++ sort.h
@@ -56,6 +56,9 @@ void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
 void max_heapify(int *array, size_t size, size_t base, size_t root);
 int get_max_int(int *array, int size);
 void radix_count_sort(int *array, size_t size, int sig, int *buff);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow);
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
 
 /* Tasks */
 void bubble_sort(int *array, size_t size);
@@ -68,5 +71,6 @@ void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 
 #endif /*end of _SORT_H_ file */
